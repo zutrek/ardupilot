dddbc577d49bbd950e051252cc87787f1670c0c4
Tamas Nepusz <ntamas@gmail.com>
2026-03-05 02:10:54 +0100

    fix: return false from debug handler when it is unused

--- libraries/AC_DroneShowManager/AC_DroneShowManager_Debug.cpp
+++ libraries/AC_DroneShowManager/AC_DroneShowManager_Debug.cpp
@@ -5,5 +5,5 @@ bool AC_DroneShowManager::_run_debug_request_handler(const mavlink_command_int_t
     // Use this function to add ad-hoc debugging code to the firmware as needed.
     // Make sure to return true if you actually use the handler; otherwise return
     // false.
-    return true;
+    return false;
 }
